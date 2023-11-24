e6f48fd51a4602029a641a23482d3a50d1a5f9fd
Bohdan <bohdan.g.54@gmail.com>
2023-11-24 16:23:59 +0200

    minor changes

--- src/main/java/org/example/alternative/AlternativeNumeralSystem.java
+++ src/main/java/org/example/alternative/AlternativeNumeralSystem.java
@@ -3,7 +3,6 @@ package org.example.alternative;
 public enum AlternativeNumeralSystem {
     GOLDEN_SYSTEM,
     SQUARE_ROOT_SYSTEM,
-//    FRACTIONAL_SYSTEM,
     FIBONACCI_SYSTEM,
     IMPERIAL_SYSTEM,
     UNARY_SYSTEM;
